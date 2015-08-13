ab4ca94feaaa2a3814fa93940591cf25aac32c5d
Joe S <joe@pixelbash.co.nz>
2015-08-14 07:48:24 +0800

    Set up logger

--- shopify_integration.rb
+++ shopify_integration.rb
@@ -4,6 +4,14 @@ require "endpoint_base"
 require_all 'lib'
 
 class ShopifyIntegration < EndpointBase::Sinatra::Base
+  ## Set up logging
+  configure {
+    enable :logging
+  }
+  before {
+    env['rack.logger'] = Logger.new "#{settings.root}/log/#{settings.environment}.log"
+  }
+
   post '/*_shipment' do # /add_shipment or /update_shipment
     summary = Shopify::Shipment.new(@payload['shipment'], @config).ship!
 
